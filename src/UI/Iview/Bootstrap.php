04b887cfa0c5d130acc8ddc72079a11c61d7e285
xaboy <xaboy2005@qq.com>
2019-10-11 13:44:00 +0800

    fix CDN 失效

--- src/UI/Iview/Bootstrap.php
+++ src/UI/Iview/Bootstrap.php
@@ -22,9 +22,9 @@ class Bootstrap implements BootstrapInterface
         $dependScript = $form->getDependScript();
 
         array_splice($dependScript, 2, 0, [
-            '<link href="https://unpkg.com/Iview@3.4.2/dist/styles/Iview.css" rel="stylesheet">',
-            '<script src="https://unpkg.com/Iview@3.4.2/dist/Iview.min.js"></script>',
-            '<script src="https://unpkg.com/@form-create/Iview@1.0.3/dist/form-create.min.js"></script>',
+            '<link href="https://unpkg.com/iview@3.4.2/dist/styles/iview.css" rel="stylesheet">',
+            '<script src="https://unpkg.com/iview@3.4.2/dist/iview.min.js"></script>',
+            '<script src="https://unpkg.com/@form-create/iview@1.0.3/dist/form-create.min.js"></script>',
         ]);
 
         $form->setDependScript($dependScript);
