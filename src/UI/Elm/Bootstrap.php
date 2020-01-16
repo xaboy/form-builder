a8a811da96e770d5232911eb323da907c5a92acd
xaboy <xaboy2005@qq.com>
2020-01-16 14:48:04 +0800

    修复 iview.frame 组件

--- src/UI/Elm/Bootstrap.php
+++ src/UI/Elm/Bootstrap.php
@@ -25,7 +25,7 @@ class Bootstrap implements BootstrapInterface
         array_splice($dependScript, 2, 0, [
             '<link href="https://unpkg.com/element-ui@2.12.0/lib/theme-chalk/index.css" rel="stylesheet">',
             '<script src="https://unpkg.com/element-ui@2.12.0/lib/index.js"></script>',
-            '<script src="https://unpkg.com/@form-create/element-ui@1.0.5/dist/form-create.min.js"></script>',
+            '<script src="https://unpkg.com/@form-create/element-ui@1.0.6/dist/form-create.min.js"></script>',
         ]);
 
         $form->setDependScript($dependScript);
